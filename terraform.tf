028e6b615f643511f574331080044a881dd45ddc
petergillhub <petergillhmg@gmail.com>
2024-02-17 22:48:11 +0000

    comitted

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_version = "~>1.6.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "demo-s3-bucket-petergill"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-dynamodb-demo"
+  }
+}
+
